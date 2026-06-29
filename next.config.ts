62c600ed4526cdabae5d5be296ce7ea59a26d9e2
sieusanh <sieusanh00@gmail.com>
2026-06-29 09:36:31 +0700

    add dependency "@next/bundle-analyzer"

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import withBundleAnalyzer from '@next/bundle-analyzer';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  cacheComponents: true,
 };
 
-export default nextConfig;
+// export default nextConfig;
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+export default bundleAnalyzer(nextConfig);
